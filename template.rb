1252ef5a2b1ac848b27a9f44762afb177eeae73c
Achmad Chun Chun <anonychun@gmail.com>
2025-05-07 08:49:44 +0700

    refactor: restructure API template

--- template.rb
+++ template.rb
@@ -1,3 +1,23 @@
+require "fileutils"
+require "shellwords"
+
+if __FILE__.match?(%r{\Ahttps?://})
+  require "tmpdir"
+  source_paths.unshift(tempdir = Dir.mktmpdir("paradis-"))
+  at_exit { FileUtils.remove_entry(tempdir) }
+  git clone: [
+    "--quiet",
+    "https://github.com/anonychun/paradis.git",
+    tempdir
+  ].map(&:shellescape).join(" ")
+
+  if (branch = __FILE__[%r{paradis/(.+)/template.rb}, 1])
+    Dir.chdir(tempdir) { git checkout: branch }
+  end
+else
+  source_paths.unshift(File.dirname(__FILE__))
+end
+
 def gem_exists?(name)
   IO.read("Gemfile") =~ /^\s*gem ['"]#{name}['"]/
 end
@@ -14,44 +34,17 @@ gem_group :development do
   add_gem "standard"
 end
 
-add_gem "ostruct"
-add_gem "dry-schema"
-add_gem "grape-entity"
-add_gem "rapidjson"
 add_gem "mission_control-jobs"
 
 copy_file "config/locales/id.yml"
 
-environment "config.i18n.default_locale = :id"
-environment "config.solid_queue.preserve_finished_jobs = false"
+application "config.solid_queue.preserve_finished_jobs = false"
 
 insert_into_file "config/routes.rb", after: "Rails.application.routes.draw do" do
   <<~RUBY.indent(2).prepend("\n")
     mount MissionControl::Jobs::Engine, at: "/jobs"
   RUBY
 end
-
-initializer "action_controller.rb", <<~RUBY
-  ActionController::Parameters.class_eval do
-    def validate!(&)
-      schema = Dry::Schema.Params do
-        config.messages.backend = :i18n
-      end
-
-      schema = schema.merge(Dry::Schema.Params(&))
-      result = schema.call(to_unsafe_h)
-      unless result.success?
-        raise ApiError.new(errors: {params: result.errors.to_h}, status: 400)
-      end
-
-      merge!(result.to_h)
-    end
-  end
-RUBY
-
-initializer "rapidjson.rb", <<~RUBY
-  ActiveSupport::JSON::Encoding.json_encoder = RapidJSON::ActiveSupportEncoder
-RUBY
 
 initializer "clear_local_log.rb", <<~RUBY
   if Rails.env.local?
@@ -60,92 +53,58 @@ initializer "clear_local_log.rb", <<~RUBY
   end
 RUBY
 
-file "app/services/application_service.rb", <<~RUBY
-  class ApplicationService
-    def self.call(**)
-      new(**).call
+initializer "generators.rb", <<~RUBY
+  Rails.application.config.generators do |g|
+    g.orm :active_record, primary_key_type: :uuid
+    g.assets false
+    g.helper false
+    g.template_engine nil
+    g.test_framework nil
+  end
+RUBY
+
+initializer "active_record.rb", <<~RUBY
+  ActiveSupport.on_load(:active_record_postgresqladapter) do
+    self.datetime_type = :timestamptz
+  end
+
+  ActiveRecord::Base.class_eval do
+    before_create :assign_id
+
+    private def assign_id
+      if self.class.name.start_with?("ActiveStorage::")
+        self.id ||= Util.generate_id
+      end
     end
   end
 RUBY
 
-file "app/entities/application_entity.rb", <<~RUBY
-  class ApplicationEntity < Grape::Entity
-    expose :id
-    expose :created_at
-    expose :updated_at
+file "app/services/service.rb", <<~RUBY
+  module Service
+    module_function
   end
 RUBY
 
 file "app/utils/util.rb", <<~RUBY
   module Util
-  end
-RUBY
+    module_function
 
-if options[:database] == "sqlite3"
-  add_gem "ulid"
-
-  initializer "active_record.rb", <<~RUBY
-    ActiveRecord::Base.class_eval do
-      before_create :assign_id
-
-      private def assign_id
-        if self.class.name.start_with?("ActiveStorage::")
-          self.id ||= Util.generate_id
-        end
-      end
-    end
-  RUBY
-
-  initializer "generators.rb", <<~RUBY
-    Rails.application.config.generators do |g|
-      g.orm :active_record, primary_key_type: :text
-      g.test_framework nil
-    end
-  RUBY
-
-  inject_into_module "app/utils/util.rb", "Util", <<~RUBY.indent(2)
-    def generate_id
-      ULID.generate
-    end
-  RUBY
-else
-  initializer "generators.rb", <<~RUBY
-    Rails.application.config.generators do |g|
-      g.orm :active_record, primary_key_type: :uuid
-      g.test_framework nil
-    end
-  RUBY
-
-  inject_into_module "app/utils/util.rb", "Util", <<~RUBY.indent(2)
     def generate_id
       SecureRandom.uuid_v7
     end
-  RUBY
-end
+  end
+RUBY
 
-if options[:database] == "postgresql"
-  initializer "active_record.rb", <<~RUBY
-    require "active_record/connection_adapters/postgresql_adapter"
-
-    ActiveRecord::ConnectionAdapters::PostgreSQLAdapter.datetime_type = :timestamptz
-    ActiveRecord::Base.time_zone_aware_types << :timestamptz
-  RUBY
-end
+file "app/errors/application_error.rb", <<~RUBY
+  class ApplicationError < StandardError
+  end
+RUBY
 
 inject_into_class "app/models/application_record.rb", "ApplicationRecord", <<~RUBY.indent(2)
   before_create :assign_id
 
   private def assign_id
     self.id ||= Util.generate_id
-  end
-RUBY
-
-inject_into_module "app/utils/util.rb", "Util", <<~RUBY.indent(2).concat("\n")
-  module_function
-RUBY
-
-file "app/errors/application_error.rb", <<~RUBY
-  class ApplicationError < StandardError
   end
 RUBY
 
@@ -162,3 +121,5 @@ TXT
 
 append_to_file ".gitignore", ignored_files
 append_to_file ".dockerignore", ignored_files
+
+apply "template/api.rb"
---
+++ template/api.rb
@@ -0,0 +1,223 @@
+add_gem "dry-schema"
+add_gem "grape-entity"
+
+initializer "action_controller.rb", <<~RUBY
+  ActionController::Parameters.class_eval do
+    def validate!(schema = nil, &)
+      schema ||= Dry::Schema.Params(&)
+      result = schema.call(to_unsafe_h)
+
+      unless result.success?
+        raise ApiError.new(
+          errors: {params: result.errors(locale: :id).to_h},
+          status: :unprocessable_entity
+        )
+      end
+
+      merge!(result.to_h)
+    end
+  end
+RUBY
+
+initializer "dry_schema.rb", <<~RUBY
+  module DryTypes
+    include Dry.Types()
+  end
+
+  TypeContainer = Dry::Schema::TypeContainer.new
+
+  TypeContainer.register(
+    "params.file",
+    DryTypes.Instance(ActionDispatch::Http::UploadedFile)
+  )
+
+  Dry::Schema.config.types = TypeContainer
+  Dry::Schema.config.messages.backend = :i18n
+RUBY
+
+insert_into_file "config/routes.rb", after: "Rails.application.routes.draw do" do
+  <<~RUBY.indent(2).prepend("\n")
+    namespace :api, defaults: {format: :json} do
+      namespace :v1 do
+      end
+    end
+  RUBY
+end
+
+file "app/models/current.rb", <<~RUBY
+  class Current < ActiveSupport::CurrentAttributes
+    attribute :api_ctx
+  end
+RUBY
+
+file "app/entities/application_entity.rb", <<~RUBY
+  class ApplicationEntity < Grape::Entity
+  end
+RUBY
+
+file "app/errors/api_error.rb", <<~RUBY
+  class ApiError < ApplicationError
+    attr_reader :status, :errors
+
+    def initialize(errors: {}, status: :internal_server_error)
+      @errors = errors.is_a?(String) ? {message: errors} : errors
+      @status = status
+    end
+  end
+RUBY
+
+file "app/helpers/api_helper.rb", <<~RUBY
+  module ApiHelper
+    def present(json, &)
+      ok = response.status >= 100 && response.status < 400
+      json.ok ok
+      json.meta Current.api_ctx[:meta].empty? ? nil : Current.api_ctx[:meta]
+
+      if ok
+        json.data(&)
+        json.errors nil
+      else
+        json.data nil
+        json.errors(&)
+      end
+    end
+  end
+RUBY
+
+file "app/views/api/_api.json.jbuilder", <<~RUBY
+  present json do
+    json.merge! payload
+  end
+RUBY
+
+lib "api/helpers/presenter.rb", <<~RUBY
+  module Api::Helpers::Presenter
+    extend ActiveSupport::Concern
+
+    included do
+      before_action do
+        Current.api_ctx = Hash.new { |h, k| h[k] = {} }
+      end
+    end
+
+    def present_meta(key, value)
+      Current.api_ctx[:meta][key] = value
+    end
+
+    def error!(errors, status = :internal_server_error)
+      raise ApiError.new(errors: errors, status: status)
+    end
+
+    def param_error!(key, *messages)
+      error!({params: {key => messages}}, :unprocessable_entity)
+    end
+
+    def present(json: nil, status: :ok)
+      render template: "api/_api", status: status, locals: {payload: json}
+    end
+
+    def present_success
+      present json: {
+        message: :ok
+      }
+    end
+  end
+RUBY
+
+lib "api/helpers/exception_handler.rb", <<~RUBY
+  module Api::Helpers::ExceptionHandler
+    extend ActiveSupport::Concern
+
+    included do
+      rescue_from StandardError do |e|
+        case e
+        when ApiError
+          status = e.status
+          errors = e.errors
+        when ActionDispatch::Http::Parameters::ParseError
+          status = :unprocessable_entity
+          errors = {message: "Permintaan tidak valid"}
+        when ActiveRecord::RecordInvalid
+          status = :unprocessable_entity
+          errors = {validations: e.record.errors.full_messages}
+        when ActiveRecord::RecordNotFound
+          status = :not_found
+          errors = {message: "\#{e.model} tidak ditemukan"}
+        else
+          status = :internal_server_error
+          errors = {message: "Terjadi kesalahan, silakan coba lagi"}
+        end
+
+        if Rails.env.local? || ENV["DEBUG"].eql?("1")
+          present_meta(:trace, {
+            class: e.class.name,
+            exception: e
+          })
+        end
+
+        present json: errors, status: status
+      end
+    end
+  end
+RUBY
+
+lib "api/helpers/paginator.rb", <<~RUBY
+  module Api::Helpers::Paginator
+    def paginate(collection)
+      params.validate! do
+        optional(:page).filled(:integer, gt?: 0)
+        optional(:per_page).filled(:integer, gt?: 0)
+        optional(:start_date).filled(:date_time)
+        optional(:end_date).filled(:date_time)
+      end
+
+      if params[:start_date].present?
+        collection = collection.where(created_at: params[:start_date]..)
+      end
+
+      if params[:end_date].present?
+        collection = collection.where(created_at: ..params[:end_date])
+      end
+
+      if params[:page].present? && params[:per_page].present?
+        total = collection.count
+        collection = collection
+          .limit(params[:per_page])
+          .offset((params[:page] - 1) * params[:per_page])
+
+        meta_pagination = {
+          page: params[:page],
+          per_page: params[:per_page],
+          total: total
+        }
+
+        present_meta :pagination, meta_pagination
+      end
+
+      collection
+    end
+  end
+RUBY
+
+lib "api/helpers.rb", <<~RUBY
+  module Api::Helpers
+    extend ActiveSupport::Concern
+
+    include Presenter
+    include ExceptionHandler
+    include Paginator
+  end
+RUBY
+
+file "app/controllers/api_controller.rb", <<~RUBY
+  class ApiController < ActionController::API
+    include ActionController::Cookies
+
+    include Api::Helpers
+  end
+RUBY
+
+file "app/controllers/api/v1_controller.rb", <<~RUBY
+  class Api::V1Controller < ApiController
+  end
+RUBY
