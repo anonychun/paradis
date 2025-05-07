fbdf1e0276db41e7dae74952651f8c11b7c8c3ed
Achmad Chun Chun <anonychun@gmail.com>
2025-05-07 18:05:44 +0700

    feat: enhance Dockerfile with tmux and dumb-init for improved process management

--- template/docker.rb
+++ template/docker.rb
@@ -7,3 +7,13 @@ inject_into_file "Dockerfile", after: "ENV RAILS_ENV=\"production\" \\" do
     SOLID_QUEUE_IN_PUMA="1" \\
   RUBY
 end
+
+inject_into_file "Dockerfile", after: "apt-get install --no-install-recommends -y curl" do
+  <<~RUBY.prepend(" ").chomp
+    tmux dumb-init
+  RUBY
+end
+
+gsub_file "Dockerfile",
+  "ENTRYPOINT [\"/rails/bin/docker-entrypoint\"]",
+  "ENTRYPOINT [\"/usr/bin/dumb-init\", \"--\", \"/rails/bin/docker-entrypoint\"]"
